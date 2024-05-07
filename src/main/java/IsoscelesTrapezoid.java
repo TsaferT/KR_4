c750d2c7af82e1bb16ac1785da09aaed2b5817f7
Dima <zebzeev14@mail.ru>
2024-05-07 12:40:39 +0500

    //

--- src/main/java/IsoscelesTrapezoid.java
+++ src/main/java/IsoscelesTrapezoid.java
@@ -19,18 +19,18 @@ public class IsoscelesTrapezoid {
         }
     }
     public boolean isIsoscelesTrapezoid() {
-        double side1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)); // Длина первой стороны
-        double side2 = Math.sqrt(Math.pow(x3 - x2, 2) + Math.pow(y3 - y2, 2)); // Длина второй стороны
-        double side3 = Math.sqrt(Math.pow(x4 - x3, 2) + Math.pow(y4 - y3, 2)); // Длина третьей стороны
-        double side4 = Math.sqrt(Math.pow(x1 - x4, 2) + Math.pow(y1 - y4, 2)); // Длина четвертой стороны
+        double side1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        double side2 = Math.sqrt(Math.pow(x3 - x2, 2) + Math.pow(y3 - y2, 2));
+        double side3 = Math.sqrt(Math.pow(x4 - x3, 2) + Math.pow(y4 - y3, 2));
+        double side4 = Math.sqrt(Math.pow(x1 - x4, 2) + Math.pow(y1 - y4, 2));
 
         return (side1 == side3);
     }
     public double calculPerimeter() {
-        double side1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)); // Длина первой стороны
-        double side2 = Math.sqrt(Math.pow(x3 - x2, 2) + Math.pow(y3 - y2, 2)); // Длина второй стороны
-        double side3 = Math.sqrt(Math.pow(x4 - x3, 2) + Math.pow(y4 - y3, 2)); // Длина третьей стороны
-        double side4 = Math.sqrt(Math.pow(x1 - x4, 2) + Math.pow(y1 - y4, 2)); // Длина четвертой стороны
+        double side1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        double side2 = Math.sqrt(Math.pow(x3 - x2, 2) + Math.pow(y3 - y2, 2));
+        double side3 = Math.sqrt(Math.pow(x4 - x3, 2) + Math.pow(y4 - y3, 2));
+        double side4 = Math.sqrt(Math.pow(x1 - x4, 2) + Math.pow(y1 - y4, 2));
         return side1 + side2 + side3 + side4;
     }
     public double calculArea() {
